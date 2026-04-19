e6e7c6ac0bff514d60697cedaf0ab7d0959338a9
mishizu0615 <129640448+mishizu0615@users.noreply.github.com>
2026-04-20 06:48:01 +0900

    Create line_utils.py

---
+++ line_utils.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+
+
+def reply_line(reply_token: str, text: str):
+    requests.post(
+        "https://api.line.me/v2/bot/message/reply",
+        headers={
+            "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "replyToken": reply_token,
+            "messages": [{"type": "text", "text": text}]
+        },
+        timeout=10
+    )
